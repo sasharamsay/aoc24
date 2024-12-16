65b058697d4f2346f137be6559b7c95bc8147dc1
Sasha Ramsay <schopelas@qualtrics.com>
2024-12-16 09:46:35 -0700

    minor formatting changes

--- day7.py
+++ day7.py
@@ -2,14 +2,7 @@
 # https://adventofcode.com/2024/day/7
 
 targets = []
-operand_list = []
-
-
-def unconcatenate(target, operand):
-    target_str = str(target)
-    remove_str = str(operand)
-    len_remove_str = len(remove_str)
-    return int(target_str[:-len_remove_str])
+operands_all = []
 
 
 def solve(target, operands, concat):  # changing approach to work backwards
@@ -18,44 +11,40 @@ def solve(target, operands, concat):  # changing approach to work backwards
     elif target - operands[-1] < 0 or len(operands) > target:
         return False
     else:
-        x = 10 ** len(str(operands[-1]))
         if target % operands[-1] == 0 and solve(int(target / operands[-1]), operands[:-1], concat):
             return True
-        if target % x == operands[-1] and concat and solve(unconcatenate(target, operands[-1]), operands[:-1], True):
+        if (concat and target % 10 ** len(str(operands[-1])) == operands[-1]
+                and solve(int(str(target)[:-len(str(operands[-1]))]), operands[:-1], True)):
             return True
         return solve(target - operands[-1], operands[:-1], concat)
 
 
 def read(filename):
     targets.clear()
-    operand_list.clear()
-
+    operands_all.clear()
     for line in open(filename, 'r').readlines():
         targets.append(int(line.split(':')[0]))
-        operand_list.append(list(int(x) for x in line.split(':')[1].removeprefix(' ').split(' ')))
+        operands_all.append(list(int(x) for x in line.split(':')[1].removeprefix(' ').split(' ')))
 
 
 def day_7(filename):
     read(filename)
     possible_sum = 0
 
-    for i, target in enumerate(targets):
-        print(str(operand_list[i]))
-        if solve(target, operand_list[i], False):
+    for target, operands in zip(targets, operands_all):
+        if solve(target, operands, False):
             possible_sum += target
-
-    print(possible_sum)
     return possible_sum
 
 
 def day_7_part_2(filename):
     read(filename)
-    possible_sum_normal = 0
-    possible_sum_extra = 0
+    possible_sum = 0
+    possible_sum_concat = 0
 
-    for i, target in enumerate(targets):
-        if solve(target, operand_list[i], False):
-            possible_sum_normal += target
-        elif solve(target, operand_list[i], True):
-            possible_sum_extra += target
-    return possible_sum_normal + possible_sum_extra
+    for target, operands in zip(targets, operands_all):
+        if solve(target, operands, False):
+            possible_sum += target
+        elif solve(target, operands, True):
+            possible_sum_concat += target
+    return possible_sum + possible_sum_concat

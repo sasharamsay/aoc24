ffc9d8c0adf0e6affcc8cc9e3c6c37959c1d867b
Sasha Ramsay <schopelas@qualtrics.com>
2024-12-16 14:12:16 -0700

    minor formatting changes

--- day4.py
+++ day4.py
@@ -8,24 +8,24 @@ def next_letter_found(start, direction, letter):
     row = start[0]
     col = start[1]
 
-    if 'north' in direction:
-        row = row - 1
-    if 'south' in direction:
-        row = row + 1
-    if 'east' in direction:
-        col = col - 1
-    if 'west' in direction:
-        col = col + 1
+    if 'N' in direction:
+        row -= 1
+    if 'S' in direction:
+        row += 1
+    if 'E' in direction:
+        col -= 1
+    if 'W' in direction:
+        col += 1
     if row > len(search) - 1 or col > len(search[0]) - 1 or row < 0 or col < 0 or search[row][col] != letter:
-        return -1, -1
+        return 'none'
 
     return row, col
 
 
-def directions_match(s, m): # same letters can't be opposite of each other
+def directions_match(s, m):  # same letters can't be opposite of each other
     s_match = False
     m_match = False
-    for direction in ('north', 'south', 'east', 'west'):
+    for direction in ('N', 'S', 'E', 'W'):
         if direction in s[0] and direction in s[1]:
             s_match = True
         if direction in m[0] and direction in m[1]:
@@ -45,7 +45,6 @@ def read(filename):
 
 def day_4(filename):
     read(filename)
-    directions = ['north', 'northeast', 'east', 'southeast', 'south', 'southwest', 'west', 'northwest']
     num_xmas = 0
     x_pos = []
 
@@ -55,21 +54,19 @@ def day_4(filename):
                 x_pos.append((i, j))
 
     for x in x_pos:
-        for direction in directions:
+        for direction in ('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'):
             pos_found = x
             for letter in ('M', 'A', 'S'):
                 pos_found = next_letter_found(pos_found, direction, letter)
-                if pos_found == (-1, -1):
+                if pos_found == 'none':
                     break
-            if pos_found != (-1, -1):
+            if pos_found != 'none':
                 num_xmas += 1
 
     return num_xmas
 
 
 def day_4_part_2(filename):
-    directions = ['northeast', 'southeast', 'southwest', 'northwest']
-
     read(filename)
     num_xmas = 0
     a_pos = []
@@ -84,14 +81,15 @@ def day_4_part_2(filename):
         s_directions = []
         m_count = 0
         m_directions = []
-        for direction in directions:
-            if next_letter_found(a, direction, 'S') != (-1, -1):
+        for direction in ('NE', 'SE', 'SW', 'NW'):
+            if next_letter_found(a, direction, 'S') != 'none':
                 s_count += 1
                 s_directions.append(direction)
-            if next_letter_found(a, direction, 'M') != (-1, -1):
+            if next_letter_found(a, direction, 'M') != 'none':
                 m_count += 1
                 m_directions.append(direction)
         if s_count == 2 and m_count == 2 and directions_match(s_directions, m_directions):
             num_xmas += 1
 
     return num_xmas
+
